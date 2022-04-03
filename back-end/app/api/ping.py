a93cfd2ef12644e203ab2125796e0a428babc45b
slhe <1357667054@qq.com>
2022-04-03 16:47:37 +0800

    1. 后端Flask RESTful API提供测试接口 Ping-Pong!

---
+++ back-end/app/api/ping.py
@@ -0,0 +1,12 @@
+from flask import jsonify
+
+from app.api import bp
+
+
+@bp.route('/ping', methods=['GET'])
+def ping():
+    """前端Vue.js用来测试与后端Flask API的连通性"""
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
